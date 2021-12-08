378edcef851ae8078105ea880f46b111694ab485
swapnadevops20 <swapnadevops20@gmail.com>
2021-12-08 17:56:45 +0530

    webdriver location

--- src/main/java/com/qa/fkart/Base/fkart_Base.java
+++ src/main/java/com/qa/fkart/Base/fkart_Base.java
@@ -12,18 +12,28 @@ public static WebDriver driver;
 	
 	public static void initialization() throws InterruptedException
 	{
-		driver = new ChromeDriver();
+		
+		
+		  System.setProperty("webdriver.chrome.driver","E:\\Downloads\\chromedriver_win32\\chromedriver.exe");
+				  
+				  driver = new ChromeDriver();
+				  
+				  driver.get("https://www.flipkart.com/");
+				 
+				
+		/*driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		//Thread.sleep(3000);
 		driver.get("https://www.flipkart.com/");
-		
+		*/
 		//pageload wait time
 				driver.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
 			
 	
 		//Implicit wait for five seconds
 		// driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+				
 			}
 		
 }
